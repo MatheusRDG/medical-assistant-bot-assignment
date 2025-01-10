9d3613e84121d4acb6cce1613bd14500fb90c678
Matheus Rodrigues <matheusrdgsf@gmail.com>
2025-01-10 16:05:36 -0300

    advanced rag added

--- notebooks/metrics.py
+++ notebooks/metrics.py
@@ -1,8 +1,22 @@
 import evaluate
 from typing import List, Dict
+from sentence_transformers import SentenceTransformer
+from wordllama import WordLlamaInference
+from numpy import ndarray
 
 bleu = evaluate.load("bleu")
 bertscore = evaluate.load("bertscore")
+model_name = "sentence-transformers/sentence-t5-base"
+model = SentenceTransformer(model_name)
+
+def unpack_y_true(y_true):
+    return [i[0] for i in y_true]
+
+def st_sim(y_pred: List[str], y_true: List[List[str]]) -> ndarray:
+        embd1 = model.encode(y_pred, show_progress_bar=False)
+        embd2 = model.encode(unpack_y_true(y_true), show_progress_bar=False)
+        return model.similarity_pairwise(embd1, embd2).numpy()
+    
 
 def compute_metrics(y_pred: List[str], y_true: List[List[str]]) -> Dict:
 
@@ -16,7 +30,10 @@ def compute_metrics(y_pred: List[str], y_true: List[List[str]]) -> Dict:
         device="cuda",
     )
 
+    st_similarities = st_sim(y_pred, y_true)
+
     return {
         "bleu": bleu_score,
         "bertscore": bertscore_score,
+        "st_similarities": st_similarities
     }
